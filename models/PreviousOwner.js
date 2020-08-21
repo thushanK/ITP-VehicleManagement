decaae3695ce46d2ede18910dabe67e3f783bbaf
Thushan <ktwimalaweera@gmail.com>
2020-08-21 16:23:00 +0530

    Initial commit

---
+++ models/PreviousOwner.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PreviousOwnerSchema = new Schema({
+    NIC: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    PreviousOwnerName: {
+        type: String,
+        required: true,
+    },
+    contactNumber: {
+        type: String,
+        required: true,
+    },
+    address: {
+        type: String,
+        required: true,
+    },
+});
+
+module.exports = PreviousOwnerSchema.mongoose.model("PreviousOwner", PreviousOwnerSchema);  
